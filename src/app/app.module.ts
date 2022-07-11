d1b8ce67023ce1d16d3985a753ff8fd5a22bf7e9
Akash Dash <akashdash2203@gmail.com>
2022-07-11 15:33:24 +0530

    Sigin Component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    SignInComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sign-in/sign-in.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CognitoService } from '../cognito.service';
+import { User } from '../User';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.scss'],
+})
+
+export class SignInComponent {
+
+  constructor(private cognitoService: CognitoService){}
+
+  public signIn(): void 
+  {
+    let user : User = {
+      email : (<HTMLInputElement>document.getElementById("email")).value,
+      password : (<HTMLInputElement>document.getElementById("password")).value
+    }
+    console.log(user)
+    this.cognitoService.signIn(user)
+  }
+}
